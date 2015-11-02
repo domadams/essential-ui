65ff9609f6eca020b1a0ec9bb2374be2d64352ee
Dom Adams <dominic.adams@uk.tesco.com>
2015-11-02 17:47:45 +0000

    Update site styles for buttons

--- site/components/buttons/index.js
+++ site/components/buttons/index.js
@@ -1,3 +1,7 @@
+if (process.env.BROWSER) {
+    require('./_buttons.scss');
+}
+
 import React, { Component } from 'react';
 import { Button } from '../../../dist';
 
@@ -7,8 +11,30 @@ class Buttons extends Component{
     render() {
         return (
             <div className="buttons-component">
-                <div>
-                    <Button size="lg" text="Hello!" type="primary" />
+                <h1>Buttons</h1>
+                <p>
+                    Examples of button component and how to use it.
+                </p>
+                <h2>Sizes</h2>
+                <div className="example">
+                    <div className="example-component">
+                        <Button size="lg" text="Large Button" type="primary" />
+                    </div>
+                    <div className="example-component">
+                        <Button size="md" text="Medium Button" type="primary" />
+                    </div>
+                    <div className="example-component">
+                        <Button size="sm" text="Small Button" type="primary" />
+                    </div>
+                </div>
+                <h2>Variants</h2>
+                <div className="example">
+                    <div className="example-component">
+                        <Button size="md" text="Button" type="primary" />
+                    </div>
+                    <div className="example-component">
+                        <Button size="md" text="Button" type="secondary" />
+                    </div>
                 </div>
             </div>
         );
