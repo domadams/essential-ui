33ab866bccaeb8a8159fd0c8e100658791b9b264
Dom Adams <dominic.adams@uk.tesco.com>
2015-11-16 16:25:47 +0000

    Use babelrc for config

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: ['babel-loader'],
                 query: {
-                    cacheDirectory: true,
-                    presets: ['es2015', 'react', 'stage-0']
+                    cacheDirectory: true
                 }
             }
         ],
