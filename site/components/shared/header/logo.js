90c1aa5e15ad852d1b140a2907cc2d39f153ee34
Dom Adams <dominic.adams@uk.tesco.com>
2015-11-18 21:30:32 +0000

    Add description to site

--- site/components/shared/header/logo.js
+++ site/components/shared/header/logo.js
@@ -5,10 +5,12 @@ class Logo extends React.Component{
     static displayName = 'Logo';
     render() {
         return (
-            <Link className="logo" to={`/`}>
-                <span className="visually-hidden">Essential UI Logo</span>
-                <span className="logo-text">ssential UI</span>
-            </Link>
+            <h1>
+                <Link className="logo" to={`/`}>
+                    <span className="visually-hidden">Essential UI Logo</span>
+                    <span className="logo-text">ssential UI</span>
+                </Link>
+            </h1>
         );
     }
 }
