131820ac05443bafee19a9c3f6783dfdf9e85cfc
Dom Adams <dominic.adams@uk.tesco.com>
2015-11-18 21:54:40 +0000

    Add titles to site

--- site/components/buttons/index.js
+++ site/components/buttons/index.js
@@ -4,178 +4,181 @@ if (process.env.BROWSER) {
 
 import React, { Component } from 'react';
 import { Button } from '../../../dist';
+import DocumentTitle from 'react-document-title';
 
 class Buttons extends Component{
     static displayName = 'Buttons';
 
     render() {
         return (
-            <div className="buttons-component">
-                <h1 className="page-header">Buttons</h1>
-                <p className="page-paragraph">
-                    Examples of button component and how to use it.
-                </p>
-                <h2 className="page-sub-header">Sizes</h2>
-                <div className="example">
-                    <div className="example-component">
-                        <Button size="lg" text="Large Button" type="primary" />
+            <DocumentTitle title="Essential UI | Buttons">
+                <div className="buttons-component">
+                    <h1 className="page-header">Buttons</h1>
+                    <p className="page-paragraph">
+                        Examples of button component and how to use it.
+                    </p>
+                    <h2 className="page-sub-header">Sizes</h2>
+                    <div className="example">
+                        <div className="example-component">
+                            <Button size="lg" text="Large Button" type="primary" />
+                        </div>
+                        <div className="example-component">
+                            <Button size="md" text="Medium Button" type="primary" />
+                        </div>
+                        <div className="example-component">
+                            <Button size="sm" text="Small Button" type="primary" />
+                        </div>
+                        <h3 className="example-usage">Example Usage:</h3>
+                        <div className="example-code">
+                            <code>
+                                <span className="code-element">
+                                    &lt;Button
+                                </span>
+                                <span className="code-attribute">
+                                    size=
+                                </span>
+                                <span className="code-value">
+                                    "lg"
+                                </span>
+                                <span className="code-attribute">
+                                    text=
+                                </span>
+                                <span className="code-value">
+                                    "Large Button"
+                                </span>
+                                <span className="code-attribute">
+                                    type=
+                                </span>
+                                <span className="code-value">
+                                    "primary"
+                                </span>
+                                <span className="code-element">
+                                    /&gt;
+                                </span>
+                            </code>
+                            <br/>
+                            <code>
+                                <span className="code-element">
+                                    &lt;Button
+                                </span>
+                                <span className="code-attribute">
+                                    size=
+                                </span>
+                                <span className="code-value">
+                                    "md"
+                                </span>
+                                <span className="code-attribute">
+                                    text=
+                                </span>
+                                <span className="code-value">
+                                    "Medium Button"
+                                </span>
+                                <span className="code-attribute">
+                                    type=
+                                </span>
+                                <span className="code-value">
+                                    "primary"
+                                </span>
+                                <span className="code-element">
+                                    /&gt;
+                                </span>
+                            </code>
+                            <br/>
+                            <code>
+                                <span className="code-element">
+                                    &lt;Button
+                                </span>
+                                <span className="code-attribute">
+                                    size=
+                                </span>
+                                <span className="code-value">
+                                    "sm"
+                                </span>
+                                <span className="code-attribute">
+                                    text=
+                                </span>
+                                <span className="code-value">
+                                    "Small Button"
+                                </span>
+                                <span className="code-attribute">
+                                    type=
+                                </span>
+                                <span className="code-value">
+                                    "primary"
+                                </span>
+                                <span className="code-element">
+                                    /&gt;
+                                </span>
+                            </code>
+                        </div>
                     </div>
-                    <div className="example-component">
-                        <Button size="md" text="Medium Button" type="primary" />
-                    </div>
-                    <div className="example-component">
-                        <Button size="sm" text="Small Button" type="primary" />
-                    </div>
-                    <h3 className="example-usage">Example Usage:</h3>
-                    <div className="example-code">
-                        <code>
-                            <span className="code-element">
-                                &lt;Button
-                            </span>
-                            <span className="code-attribute">
-                                size=
-                            </span>
-                            <span className="code-value">
-                                "lg"
-                            </span>
-                            <span className="code-attribute">
-                                text=
-                            </span>
-                            <span className="code-value">
-                                "Large Button"
-                            </span>
-                            <span className="code-attribute">
-                                type=
-                            </span>
-                            <span className="code-value">
-                                "primary"
-                            </span>
-                            <span className="code-element">
-                                /&gt;
-                            </span>
-                        </code>
-                        <br/>
-                        <code>
-                            <span className="code-element">
-                                &lt;Button
-                            </span>
-                            <span className="code-attribute">
-                                size=
-                            </span>
-                            <span className="code-value">
-                                "md"
-                            </span>
-                            <span className="code-attribute">
-                                text=
-                            </span>
-                            <span className="code-value">
-                                "Medium Button"
-                            </span>
-                            <span className="code-attribute">
-                                type=
-                            </span>
-                            <span className="code-value">
-                                "primary"
-                            </span>
-                            <span className="code-element">
-                                /&gt;
-                            </span>
-                        </code>
-                        <br/>
-                        <code>
-                            <span className="code-element">
-                                &lt;Button
-                            </span>
-                            <span className="code-attribute">
-                                size=
-                            </span>
-                            <span className="code-value">
-                                "sm"
-                            </span>
-                            <span className="code-attribute">
-                                text=
-                            </span>
-                            <span className="code-value">
-                                "Small Button"
-                            </span>
-                            <span className="code-attribute">
-                                type=
-                            </span>
-                            <span className="code-value">
-                                "primary"
-                            </span>
-                            <span className="code-element">
-                                /&gt;
-                            </span>
-                        </code>
+                    <h2 className="page-sub-header">Variants</h2>
+                    <div className="example">
+                        <div className="example-component">
+                            <Button size="md" text="Button" type="primary" />
+                        </div>
+                        <div className="example-component">
+                            <Button size="md" text="Button" type="secondary" />
+                        </div>
+                        <h3 className="example-usage">Example Usage:</h3>
+                        <div className="example-code">
+                            <code>
+                                <span className="code-element">
+                                    &lt;Button
+                                </span>
+                                <span className="code-attribute">
+                                    size=
+                                </span>
+                                <span className="code-value">
+                                    "md"
+                                </span>
+                                <span className="code-attribute">
+                                    text=
+                                </span>
+                                <span className="code-value">
+                                    "Button"
+                                </span>
+                                <span className="code-attribute">
+                                    type=
+                                </span>
+                                <span className="code-value">
+                                    "primary"
+                                </span>
+                                <span className="code-element">
+                                    /&gt;
+                                </span>
+                            </code>
+                            <br/>
+                            <code>
+                                <span className="code-element">
+                                    &lt;Button
+                                </span>
+                                <span className="code-attribute">
+                                    size=
+                                </span>
+                                <span className="code-value">
+                                    "md"
+                                </span>
+                                <span className="code-attribute">
+                                    text=
+                                </span>
+                                <span className="code-value">
+                                    "Button"
+                                </span>
+                                <span className="code-attribute">
+                                    type=
+                                </span>
+                                <span className="code-value">
+                                    "secondary"
+                                </span>
+                                <span className="code-element">
+                                    /&gt;
+                                </span>
+                            </code>
+                        </div>
                     </div>
                 </div>
-                <h2 className="page-sub-header">Variants</h2>
-                <div className="example">
-                    <div className="example-component">
-                        <Button size="md" text="Button" type="primary" />
-                    </div>
-                    <div className="example-component">
-                        <Button size="md" text="Button" type="secondary" />
-                    </div>
-                    <h3 className="example-usage">Example Usage:</h3>
-                    <div className="example-code">
-                        <code>
-                            <span className="code-element">
-                                &lt;Button
-                            </span>
-                            <span className="code-attribute">
-                                size=
-                            </span>
-                            <span className="code-value">
-                                "md"
-                            </span>
-                            <span className="code-attribute">
-                                text=
-                            </span>
-                            <span className="code-value">
-                                "Button"
-                            </span>
-                            <span className="code-attribute">
-                                type=
-                            </span>
-                            <span className="code-value">
-                                "primary"
-                            </span>
-                            <span className="code-element">
-                                /&gt;
-                            </span>
-                        </code>
-                        <br/>
-                        <code>
-                            <span className="code-element">
-                                &lt;Button
-                            </span>
-                            <span className="code-attribute">
-                                size=
-                            </span>
-                            <span className="code-value">
-                                "md"
-                            </span>
-                            <span className="code-attribute">
-                                text=
-                            </span>
-                            <span className="code-value">
-                                "Button"
-                            </span>
-                            <span className="code-attribute">
-                                type=
-                            </span>
-                            <span className="code-value">
-                                "secondary"
-                            </span>
-                            <span className="code-element">
-                                /&gt;
-                            </span>
-                        </code>
-                    </div>
-                </div>
-            </div>
+            </DocumentTitle>
         );
     }
 }
--- site/components/home/index.js
+++ site/components/home/index.js
@@ -3,21 +3,24 @@ if (process.env.BROWSER) {
 }
 
 import React, { Component } from 'react';
+import DocumentTitle from 'react-document-title';
 
 class Home extends Component{
     static displayName = 'Home';
 
     render() {
         return (
-            <div className="home-component">
-                <div className="intro">
-                    <h2 className="main-title">Essential UI</h2>
-                    <h3 className="sub-title">A UI Toolkit for React.js Websites</h3>
-                    <a className="view-github" href="https://github.com/domadams/essential-ui">
-                        View the project on GitHub
-                    </a>
+            <DocumentTitle title="Essential UI | Home">
+                <div className="home-component">
+                    <div className="intro">
+                        <h2 className="main-title">Essential UI</h2>
+                        <h3 className="sub-title">A UI Toolkit for React.js Websites</h3>
+                        <a className="view-github" href="https://github.com/domadams/essential-ui">
+                            View the project on GitHub
+                        </a>
+                    </div>
                 </div>
-            </div>
+            </DocumentTitle>
         );
     }
 }
