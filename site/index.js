af695603f859c97dccb25106689846a818f620a0
Dom Adams <dominic.adams@uk.tesco.com>
2015-11-25 15:32:57 +0000

    Update site to include react for prod

--- site/index.js
+++ site/index.js
@@ -1,3 +1,10 @@
 delete process.env.BROWSER;
 require('babel/register');
+if (process.env.NODE_ENV === 'production') {
+    require('react/dist/react.min'); // prime cache
+    require.cache[require.resolve('react')] = require.cache[require.resolve('react/dist/react.min')];
+
+    require('react-dom/dist/react-dom.min'); // prime cache
+    require.cache[require.resolve('react-dom')] = require.cache[require.resolve('react-dom/dist/react-dom.min')];
+}
 require('./server');
